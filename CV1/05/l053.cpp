26356b4e7cc3ad5a794577ab961cb654c6ec3b95
Zac Baker <zacattackbaker@gmail.com>
2024-01-11 11:55:59 -0500

    running into indexing problem in nms

--- CV1/05/l053.cpp
+++ CV1/05/l053.cpp
@@ -54,8 +54,7 @@ vector<vector<int>> sobelOperatorY(vector<vector<int>> pixelsGrey) {
 vector<vector<int>> sobelOperatorMagnitude(vector<vector<int>> pixelsGrey) {
   vector<vector<int>> pixelsSobelX = sobelOperatorX(pixelsGrey);
   vector<vector<int>> pixelsSobelY = sobelOperatorY(pixelsGrey);
-  vector<vector<int>> pixelsSobel;
-  pixelsSobel = pixelsGrey;
+  vector<vector<int>> pixelsSobel(pixelsGrey.size(), vector<int>(pixelsGrey[0].size(), 0));
   for (int i = 0; i < pixelsGrey.size(); i++) {
     for (int j = 0; j < pixelsGrey[i].size(); j++) {
       pixelsSobel[i][j] = sqrt(pow(pixelsSobelX[i][j], 2) + pow(pixelsSobelY[i][j], 2));
@@ -67,7 +66,7 @@ vector<vector<int>> sobelOperatorMagnitude(vector<vector<int>> pixelsGrey) {
 vector<vector<double>> sobelOperatorDirection(vector<vector<int>> pixelsGrey) {
   vector<vector<int>> pixelsSobelX = sobelOperatorX(pixelsGrey);
   vector<vector<int>> pixelsSobelY = sobelOperatorY(pixelsGrey);
-  vector<vector<double>> pixelsDirection;
+  vector<vector<double>> pixelsDirection(pixelsGrey.size(), vector<double>(pixelsGrey[0].size(), 0.0));
   for (int i = 0; i < pixelsGrey.size(); i++) {
     for (int j = 0; j < pixelsGrey[i].size(); j++) {
       pixelsDirection[i][j] = atan2(pixelsSobelY[i][j], pixelsSobelX[i][j]);
@@ -128,24 +127,67 @@ vector<vector<int>> hysteresisAlgorithm(vector<vector<int>> pixelsGrey, int lowT
 }
 
 vector<vector<int>> nonMaximumSuppression(vector<vector<int>> magnitude, vector<vector<double>> direction) {
+  // for (int i = 0; i < direction.size(); i++) {
+  //   for (int j = 0; j < direction[i].size(); j++) {
+  //     cout << direction[i][j] << endl;
+  //   }
+  // }
   vector<vector<int>> nms = magnitude;
-  for (int i = 0; i < direction.size(); i++) {
-    for (int j = 0; j < direction[i].size(); j++) {
+  for (int i = 0; i < magnitude.size(); i++) {
+    for (int j = 0; j < magnitude[0].size(); j++) {
+      // cout << direction[i][j] << endl;
+      cout << j << " ";
       if ((i == 0) || (j == 0) || (i == direction.size() - 1) || (j = direction[i].size() - 1)) {
-        continue;
-      }
-      if (direction[i][j] >= -(acos(-1)/8) && direction[i][j] < (acos(-1)/8)) { 
-        direction[i][j] = 0; 
+        nms[i][j] = 0;
+      } else if (((direction[i][j] >= (acos(-1)/8)) && (direction[i][j] < ((3*acos(-1))/8))) || ((direction[i][j] >= -((7*acos(-1)/8))) && (direction[i][j] < ((5*acos(-1))/4)))) { 
+        // direction[i][j] = acos(-1)/4; 
+        if ((magnitude[i][j] >= magnitude[i-1][j-1]) && (magnitude[i][j] >= magnitude[i+1][j+1])) {
+          nms[i][j] = 255;
+        } else {
+          nms[i][j] = 0;
+        }
+      } else if ((direction[i][j] >= ((3*acos(-1))/8) && direction[i][j] < ((5*acos(-1))/8)) || (direction[i][j] >= -((5*acos(-1))/8) && direction[i][j] < ((3*acos(-1))/8))) { 
+        // direction[i][j] = acos(-1)/2; 
+        if ((magnitude[i][j] >= magnitude[i-1][j]) && (magnitude[i][j] >= magnitude[i+1][j])) {
+          nms[i][j] = 255;
+        } else {
+          nms[i][j] = 0;
+        }
+      } else if ((direction[i][j] >= ((5*acos(-1))/8) && direction[i][j] < ((7*acos(-1))/8)) || (direction[i][j] >= -((3*acos(-1))/8) && direction[i][j] < -(acos(-1)/8))) { 
+        // direction[i][j] = (3*acos(-1))/4; 
+        if ((magnitude[i][j] >= magnitude[i-1][j+1]) && (magnitude[i][j] >= magnitude[i+1][j-1])) {
+          nms[i][j] = 255;
+        } else {
+          nms[i][j] = 0;
+        }
+      } else if (direction[i][j] >= ((7*acos(-1))/8) || direction[i][j] < -((7*acos(-1))/8) || (direction[i][j] >= -(acos(-1)/8) && direction[i][j] < (acos(-1)/8))) { 
+        // direction[i][j] = acos(-1); 
+        if ((magnitude[i][j] >= magnitude[i][j-1]) && (magnitude[i][j] >= magnitude[i][j+1])) {
+          nms[i][j] = 255;
+        } else {
+          nms[i][j] = 0;
+        }
       } 
-      else if (direction[i][j] >= (acos(-1)/8) && direction[i][j] < ((3*acos(-1))/8)) { direction[i][j] = acos(-1)/4; } 
-      else if (direction[i][j] >= ((3*acos(-1))/8) && direction[i][j] < ((5*acos(-1))/8)) { direction[i][j] = acos(-1)/2; } 
-      else if (direction[i][j] >= ((5*acos(-1))/8) && direction[i][j] < ((7*acos(-1))/8)) { direction[i][j] = (3*acos(-1))/4; } 
-      else if (direction[i][j] >= ((7*acos(-1))/8) || direction[i][j] < -((7*acos(-1))/8)) { direction[i][j] = acos(-1); }
-      else if (direction[i][j] >= -((3*acos(-1))/8) && direction[i][j] < -(acos(-1)/8)) { direction[i][j] = -acos(-1)/4; }
-      else if (direction[i][j] >= -((5*acos(-1))/8) && direction[i][j] < ((3*acos(-1))/8)) { direction[i][j] = -acos(-1)/2; }
-      else if (direction[i][j] >= -((7*acos(-1)/8))) { direction[i][j] = -(3*acos(-1))/4; }
+    }
+    cout << endl;
+    cout << i << endl;
+  }
+
+  return nms;
+}
+
+vector<vector<int>> combinedHysteresisNMS(vector<vector<int>> nms, vector<vector<int>> hysteresis) {
+  vector<vector<int>> combined = nms;
+  for (int i = 0; i < nms.size(); i++) {
+    for (int j = 0; j < nms[i].size(); j++) {
+      if (nms[i][j] == 255 && hysteresis[i][j] == 255) {
+        combined[i][j] = 255;
+      } else {
+        combined[i][j] = 0;
+      }
     }
   }
+  return combined;
 }
 
 void part1() {
@@ -250,8 +292,8 @@ void part2(int argc, char* argv[]) {
 }
 
 void part3(int argc, char* argv[]) {
-  int lowThreshold = 75;
-  int highThreshold = 100;
+  int lowThreshold = 0;
+  int highThreshold = 1;
   const char* inPPM = "image.ppm";
   const char* outPPM = "image1.ppm";
   const char* greyOutPPM = "imageg.ppm";
@@ -297,9 +339,12 @@ void part3(int argc, char* argv[]) {
   vector<vector<int>> pixelsSobel = sobelOperatorMagnitude(pixelsGrey);
   vector<vector<double>> pixelsDirection = sobelOperatorDirection(pixelsGrey);
   vector<vector<int>> pixelsCanny = hysteresisAlgorithm(pixelsGrey, lowThreshold, highThreshold);
+  set<int> seen = {};
+  vector<vector<int>> pixelsRecurCanny = hysteresisAlgorithmRecursion(pixelsCanny, seen);
   vector<vector<int>> pixelsNMS = nonMaximumSuppression(pixelsSobel, pixelsDirection);
+  vector<vector<int>> pixelsFinal = combinedHysteresisNMS(pixelsNMS, pixelsRecurCanny);
 
-  ofstream GreyPPM(outPPM);
+  ofstream GreyPPM(greyOutPPM);
   GreyPPM << "P3 " << width << " " << height << " " << maxval << endl;
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++){
@@ -308,14 +353,32 @@ void part3(int argc, char* argv[]) {
   }
   GreyPPM.close();
 
-  ofstream SobelPPM(greyOutPPM);
+  ofstream SobelPPM(outPPM);
   SobelPPM << "P3 " << width << " " << height << " " << maxval << endl;
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++){
-      SobelPPM << pixelsCanny[i][j] << " " << pixelsCanny[i][j] << " " << pixelsCanny[i][j] << endl;
+      SobelPPM << pixelsRecurCanny[i][j] << " " << pixelsRecurCanny[i][j] << " " << pixelsRecurCanny[i][j] << endl;
     }
   }
   SobelPPM.close();
+
+  ofstream NMSPPM(nmsOutPPM);
+  NMSPPM << "P3 " << width << " " << height << " " << maxval << endl;
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++){
+      NMSPPM << pixelsNMS[i][j] << " " << pixelsNMS[i][j] << " " << pixelsNMS[i][j] << endl;
+    }
+  }
+  NMSPPM.close();
+
+  ofstream FinalPPM(finalOutPPM);
+  FinalPPM << "P3 " << width << " " << height << " " << maxval << endl;
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++){
+      FinalPPM << pixelsFinal[i][j] << " " << pixelsFinal[i][j] << " " << pixelsFinal[i][j] << endl;
+    }
+  }
+  FinalPPM.close();
 }
 
 int main(int argc, char* argv[]) {
